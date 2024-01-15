1e8258263612b47942d05eb5f82f8d0aaee6606b
Dmitry Patin <multik.patin@yandex.ru>
2024-01-15 23:40:47 +0300

    Реализовано сохранение и выгрузка с удалением

--- nalog/checker/views.py
+++ nalog/checker/views.py
@@ -1,16 +1,27 @@
+import os
+from http import HTTPStatus
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    # post_list = Post.objects.all()
-    # page_obj = get_pages(request, post_list, POSTS_STR)
-
-    context = {
-        # "page_obj": page_obj,
-    }
-    return render(request, "checker/index.html", context)
+    if request.method == "POST" and request.FILES:
+        file = request.FILES["myfile1"]
+        fs = FileSystemStorage()
+        filename = fs.save("edite.xlsx", file)
+        file_url = fs.url(filename)
+        return render(request, "checker/index.html", {"file_url": file_url})
+    return render(request, "checker/index.html")
 
 
 def download(request: HttpRequest) -> HttpResponse:
-    pass
+    data = open(f"{settings.MEDIA_ROOT}/edite.xlsx", "rb").read()
+    response = HttpResponse(
+        data, content_type="application/vnd.ms-excel", status=HTTPStatus.OK
+    )
+    response["Content-Disposition"] = 'attachment; filename="edite.xlsx"'
+    os.remove(f"{settings.MEDIA_ROOT}/edite.xlsx")
+    return response
